b98f733819743aa7e3ca215a45e79c5f652b258f
Peng Wang <pwan0722@uni.sydney.edu.au>
2024-06-01 12:43:15 +1000

    sorted the readme file and inspiration code

--- pwan0722/README.md
+++ pwan0722/README.md
@@ -1,1 +1,19 @@
 # pwan0722_CreativeCodingMajorProject
+
+
+## **Part 1：Instructions on how to interact with the work**
+
+### Input consists of three parts: 
+1. Click the mouse to switch between day and night: After pressing the mouse, the day and night will be switched, the building windows will change, and the sun will become the moon and vice versa.
+2. Press the keyboard to make it rain,
+3. Move the mouse up and down to change the lighting range
+
+
+## **Part 2：Details of your individual approach to animating the group code.**
+
+1. I selected User Input. our group were worked on the Claude Monet, Saint Georges Majeur au Crépuscule,
+
+2. First, I distinguished between day and night in the building class and controlled the windows in different states. Then, to create the effect of falling raindrops, I created a rain class and used the [constructor](https://www.w3schools.com/java/java_constructors.asp) to define the initial position and speed of the raindrops. Then I drew the raindrops to create the effect of rain after pressing the space bar. To further refine the effect of day and night, I added a sky class, defined it using the constructor, and used [blendMode (ADD)](https://p5js.org/reference/#/p5/blendMode). The colors of overlapping pixels are added. This creates brighter, more intense colors because the color values ​​of the source and destination are added together. And the range of light changes with the mouse up and down.
+
+3. my [inspiration](https://www.reddit.com/r/gamemaker/comments/f9bxbz/tip_you_can_use_the_add_blend_mode_for_some_quick/) comes from a reddit,This reddit post helped me figure out how to create lighting effects to achieve the effects of sunlight and moonlight.
+![An image of my inspiration](Inspiration.png)
--- pwan0722/classes/sky.js
+++ pwan0722/classes/sky.js
@@ -1,8 +1,8 @@
 class Sky {
   constructor() {
-    //表示是否白天
+    //Indicate whether it is daytime.inspiration（https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/constructor）
     this.dayTime = true
-    // 日月的旋转角度
+    // The rotation angle of the sun and moon.
     this.angle = 0
   }
   display() {
@@ -11,7 +11,7 @@ class Sky {
     rect(-width/2,-height/2,width,height)
     this.sun()
     this.moon()
-    // 让旋转角度根据在白天和夜晚的情况下去进行不同的变化
+    // Make the rotation angle change differently depending on whether it is daytime or nighttime.
     if (this.dayTime) {
       this.angle = lerp(this.angle, 0, 0.1)
     } else {
@@ -24,23 +24,23 @@ class Sky {
   sun() {
     push()
     fill(250, 140, 60)
-    // 计算太阳的y坐标
+    // y-coordinate of the sun.
     let sunY = sin(this.angle - 90) * height / 2 * 0.94
-    // 当太阳在海平面上的时候
+    // When the sun is on the horizon.
     if (sunY < 0) {
-      // 计算太阳的x坐标
+      // the x-coordinate of the sun.
       let sunX = cos(this.angle - 90) * width / 2 * 2
       let sunSize = 100
       translate(sunX, sunY)
       ellipse(0, 0, sunSize, sunSize)
-      // 改变混合模式
+      // Change the blend mode.inspiration：https://p5js.org/reference/#/p5/blendMode
       blendMode(ADD)
       for (let i = 0; i < 20; i++) {
-        // 绘制太阳的射线
+        // Draw the rays of the sun.
         let angle = map(i, 0, 20, 0, 360)
         let lineX1 = cos(angle) * 55
         let lineY1 = sin(angle) * 55
-        // 射线的长度
+        // The length of the rays.
         let leng = 120 + sin(angle * 10 + frameCount * 5) * 10
         let lineX2 = cos(angle) * leng
         let lineY2 = sin(angle) * leng
@@ -51,7 +51,7 @@ class Sky {
       for (let i = 0; i < 20; i++) {
         fill(255, 90, 50, 8)
         noStroke()
-        // 绘制太阳的光照
+        // Draw the sunlight.
         let maxRadius = map(mouseY, 0, height, 3, 18)
         let radius = map(i, 0, 20, sunSize, sunSize * random(maxRadius, maxRadius + 0.5))
         ellipse(0, 0, radius, radius)
@@ -63,19 +63,19 @@ class Sky {
   moon() {
     push()
     fill(255, 230, 160)
-    // 计算月亮的y坐标
+    // Calculate the y-coordinate of the moon.
     let moonY = sin(this.angle + 90) * height / 2 * 0.94
     if (moonY < 0) {
-      // 计算太阳的x坐标
+      // Calculate the x-coordinate of the sun.
 
       let moonX = cos(this.angle + 90) * width / 2 * 2
       let moonSize = 100
       translate(moonX, moonY)
       ellipse(0, 0, moonSize, moonSize)
-      // 改变混合模式
+      // Change the blend mode.
 
       blendMode(ADD)
-      // 绘制月亮的光照
+      // Draw the moonlight.
       for (let i = 0; i < 10; i++) {
         fill(255, 230, 160, 8)
         noStroke()
